2c100e9e259f6f3463004295a7f174e43d5976e0
Lenny Evans <lenny.evans3@gmail.com>
2016-04-18 01:42:22 -0500

    Theme implementation in progress.

--- billsapp/App_Start/BundleConfig.cs
+++ billsapp/App_Start/BundleConfig.cs
@@ -5,24 +5,62 @@ namespace billsapp {
     public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            // Example (true = include sub folders)
+            // bundles.Add(new ScriptBundle("~/bundles/scripts").IncludeDirectory(
+                        //"~/Scripts", "*.js", true));
+            
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jqueryjs").Include(
+                        "~/assets/vendor/jquery/jquery.js"));
+            
+            // jQuery Validate
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvaljs").Include(
+                        "~/assets/vendor/jquery-validation/jquery.validate.js",
+                        "~/assets/vendor/jquery-validation/additional-methods.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
 
+            // jQuery UI
+            bundles.Add(new ScriptBundle("~/bundles/jqueryuijs").Include(
+                        "~/assets/vendor/jquery-ui/jquery-ui.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/jqueryuicss").IncludeDirectory(
+                        "~/assets/vendor/jquery-ui", "*.css", true));
+
+            // Nanoscroller
+            bundles.Add(new ScriptBundle("~/bundles/nanoscrollerjs").Include(
+                        "~/assets/vendor/nanoscroller/nanoscroller.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/nanoscrollercss").Include(
+                        "~/assets/vendor/nanoscroller/nanoscroller.css"));
+
+            // Modernizr
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizrjs").Include(
+                        "~/assets/vendor/modernizr/modernizr.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                      "~/assets/vendor/bootstrap/js/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/bootstrapcss").Include(
+                        "~/assets/vendor/bootstrap/css/bootstrap.css",
+                        "~/assets/vendor/bootstrap/css/bootstrap-theme.css"));
+
+            // Theme
+            bundles.Add(new StyleBundle("~/bundles/themecss").Include(
+                      "~/assets/stylesheets/theme.css",                     // Basic layout structure styles
+                      "~/assets/stylesheets/theme-custom.css",              // Customize the theme in this file
+                      "~/assets/stylesheets/skins/default.css",             // Skin
+                      "~/assets/stylesheets/skins/extension.css",
+                      "~/assets/stylesheets/skins/square-borders.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/themejs").Include(
+                      "~/assets/javascripts/theme.js",
+                      "~/assets/javascripts/theme.custom.js",
+                      "~/assets/javascripts/theme.init.js"));
         }
     }
 }
