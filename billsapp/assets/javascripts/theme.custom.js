36d5be2fb97e77db4f1270dfa22cee9e62458b6a
Lenny Evans <lenny.evans3@gmail.com>
2016-04-21 12:50:34 -0500

    Fix GitHubActivity InnerHTML error, active/expanded nav items.

--- billsapp/assets/javascripts/theme.custom.js
+++ billsapp/assets/javascripts/theme.custom.js
@@ -1,11 +1,40 @@
 /* Add here all your JS customizations */
 
-/* GitHub Activity Stream */
 $(document).ready(function () {
-    GitHubActivity.feed({
-        username: "levans88",
-        //repository: "your-repo", // optional
-        selector: "#feed"
-        //limit: 20 // optional
-    });
+
+    // Preserve scroll position
+    if (typeof localStorage !== 'undefined') {
+	    if (localStorage.getItem('sidebar-left-position') !== null) {
+		    var initialPosition = localStorage.getItem('sidebar-left-position'),
+			    sidebarLeft = document.querySelector('#sidebar-left .nano-content');
+
+		    sidebarLeft.scrollTop = initialPosition;
+	    }
+    }
+
+	// GitHub activity stream
+    if (urlPath == "/") {
+    	GitHubActivity.feed({
+	        username: "levans88",
+	        //repository: "your-repo", // optional
+	        selector: "#feed"
+	        //limit: 20 // optional
+
+    	});
+    }
+
+    // Clear nav-active class from nav menu
+    if ( $('nav > ul.nav > li.nav-active').length ) {
+    	$('nav > ul.nav > li.nav-active').removeClass('nav-active');
+    }
+
+    // Set nav-active class in nav menu for current URL path
+    if (urlPath == "/Manage") $('#nav-examples, #nav-manage').addClass('nav-active nav-expanded');
+    if (urlPath == "/Account/Login") $('#nav-login').addClass('nav-active');
+    if (urlPath == "/Account/Register") $('#nav-register').addClass('nav-active');
+
+    // Set home nav item as nav-active if no menu items are highlighted or path is "/"
+    if ( !$('nav > ul.nav > li.nav-active' ).length || urlPath == "/") {
+    	$('#nav-home').addClass('nav-active');
+    }
 });
